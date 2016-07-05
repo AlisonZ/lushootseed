b492e5eb3ef15f5f35390b7d75671d8865bbb5d8
Derek Hewitt <derekmhewitt@gmail.com>
2016-07-05 10:24:53 -0700

    js work

--- js/learn.js
+++ js/learn.js
@@ -8,21 +8,24 @@ var liElLus = document.getElementById('lushootseed_translation');
 var buttonNextFlashcard = document.getElementById('next_flashcard');
 var buttonTakeTest = document.getElementById('take_test');
 
-function Flashcard(eng, lush, url, idNum) {
+buttonNextFlashcard.addEventListener('click', displayFlashcard);
+
+function Flashcard(eng, lush, url, idNum, sound, phonetic) {
   this.english = eng;
   this.lushootseed = lush;
   this.pictureurl = url;
   this.idNum = idNum;
-  this.sound = 0;
+  this.sound = sound;
+  this.phonetic = phonetic;
   this.shown = false;
-  this.push(arrayOfFlashcards);
+  arrayOfFlashcards.push(this);
 }
 
 //make Flashcard objects here.  For now these are test objects
 function makeFlashcards() {
-  new Flashcard('englishtranslation1', 'lushootseed translation1', 'image/tester_images/coffee_test1.jpg', 1);
-  new Flashcard('englishtranslation2', 'lushootseed translation2', 'image/tester_images/door_window.jpeg', 2);
-  new Flashcard('englishtranslation3', 'lushootseed translation3', 'image/tester_images/pexels-photo-96469.jpeg', 3);
+  new Flashcard('englishtranslation1', 'lushootseed translation1', 'image/tester_images/coffee_test1.jpg', 1, 0, 0);
+  new Flashcard('englishtranslation2', 'lushootseed translation2', 'image/tester_images/door_window.jpeg', 2, 0, 0);
+  new Flashcard('englishtranslation3', 'lushootseed translation3', 'image/tester_images/pexels-photo-96469.jpeg', 3, 0, 0);
 }
 
 //randomly select Flashcard by id # for display to the user, display it, and change it's shown property to True, and save array to local storage
@@ -30,22 +33,23 @@ var generateRandomNumber = function() {
   return Math.floor(Math.random() * (arrayOfFlashcards.length));
 };
 
-function dislayFlashcard() {
-  var placeholder = generateRandomNumber;
+function displayFlashcard() {
+  var placeholder = generateRandomNumber();
   arrayOfFlashcards[placeholder].shown = true;
   imgEl.src = arrayOfFlashcards[placeholder].pictureurl;
   liElEng.textContent = arrayOfFlashcards[placeholder].english;
   liElLus.textContent = arrayOfFlashcards[placeholder].lushootseed;
+  numFlashcardsShown++;
+  localStorage.setItem('arrayOfFlashcards', JSON.stringify(arrayOfFlashcards));
+  showTestButton();
 }
 
-//button on the page should advance to the next picture
-
-function nextFlashcard() {
+function showTestButton() {
+  if(numFlashcardsShown > 6) {
+    buttonTakeTest.className = 'show_take_test';
+  }
 }
-// <button id="next_flashcard">Next Flashcard</button>
-// <button id="take_test">Take a quiz!</button>
-
-//When shown = true > 6 show button for the test page
 
 //call functions
 makeFlashcards();
+displayFlashcard();
